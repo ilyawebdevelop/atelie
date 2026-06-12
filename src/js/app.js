e5a2c55e50a59b73cdf21e9c47ab7cd766cf8a2f
илья бескаравайный <ilia966@tut.by>
2026-06-12 12:36:57 +0300

    asd

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 import * as flsFunctions from "./modules/functions.js";
 import "./modules/jquery-3.7.1.min.js";
-import "./modules/fslightbox.js";
-import "./modules/bootstrap.bundle.min.js";
+
 import './components.js';
 
 flsFunctions.isWebp();
@@ -10,6 +9,32 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar } from 'swiper';
 Swiper.use([Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar]);
 
-
+// Инициализация слайдера introSlider
+const introSlider = document.querySelector('.introSlider');
+var mySwiperIntro = new Swiper(introSlider, {
+  slidesPerView: 1,
+  speed: 1500,
+  spaceBetween: 10,
+  // autoHeight: 'true',
+  loop: true,
+  effect: 'fade',
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false, // Не останавливать после клика
+  },
+  // loop: true,
+  fadeEffect: {
+    crossFade: true
+  },
+  navigation: {
+    prevEl: introSlider?.querySelector('.navArrowPrev'),
+    nextEl: introSlider?.querySelector('.navArrowNext'),
+  },
+  pagination: {
+    el: introSlider?.querySelector('.swiper-pagination'),
+    clickable: true,
+    type: 'bullets',
+  },
+});
 
 
